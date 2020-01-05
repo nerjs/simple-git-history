a0863acc9b95d85d2a9da6afe173ab3710ab5bc8
nerjs <karifan.stap@gmail.com>
2020-01-06 00:23:36 +0200

    remove trash

--- renderer/src/app.js
+++ renderer/src/app.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 
 const StylesContainer = styled.div`
@@ -6,16 +6,9 @@ const StylesContainer = styled.div`
 `
 
 const App = () => {
-    const [Test, setTest] = useState()
-
-    useEffect(() => {
-        import('./as').then(r => setTest(() => r.default))
-    }, [setTest])
-
     return (
         <StylesContainer>
             test 2<hr />
-            {Test && <Test z="qwerty" />}
         </StylesContainer>
     )
 }
